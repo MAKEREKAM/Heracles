99ecb27beac419d99e5af1c49ee01f6c9792f2ab
MaIcer <116092283+MAKEREKAM@users.noreply.github.com>
2024-09-06 11:28:45 +0900

    Add cice property

--- src/main/java/kr/vanilage/main/nation/Cice.kt
+++ src/main/java/kr/vanilage/main/nation/Cice.kt
@@ -1,4 +1,15 @@
 package kr.vanilage.main.nation
 
-class Cice {
+class Cice(owner : Nation, x : Int, y : Int) {
+    // Default Information
+    val owner : Nation by lazy { owner }
+    val x : Int by lazy { x }
+    val y : Int by lazy { y }
+
+    // Building Information
+    var base = 0
+    var privateFactory = 0
+    var munitionFactory = 0
+    var residence = 0
+    var buildingSlot = 1
 }
