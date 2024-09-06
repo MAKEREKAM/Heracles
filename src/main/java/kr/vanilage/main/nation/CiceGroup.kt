e6cfce2dc3be5ff0c841a42429c7e7ceaec0afc0
MaIcer <116092283+MAKEREKAM@users.noreply.github.com>
2024-09-06 16:43:37 +0900

    Add cice group code

--- src/main/java/kr/vanilage/main/nation/CiceGroup.kt
+++ src/main/java/kr/vanilage/main/nation/CiceGroup.kt
@@ -1,4 +1,13 @@
 package kr.vanilage.main.nation
 
 class CiceGroup {
+    val table = HashMap<Int, HashMap<Int, Cice>>()
+
+    fun addCice(x : Int, y : Int, cice : Cice) {
+        if (!table.containsKey(x)) {
+            table[x] = HashMap()
+        }
+
+        table[x]!![y] = cice
+    }
 }
