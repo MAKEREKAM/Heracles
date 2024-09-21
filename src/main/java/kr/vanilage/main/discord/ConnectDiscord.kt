362bfeccb25404b02db6c7db04e36ff87415b280
MaIcer <116092283+MAKEREKAM@users.noreply.github.com>
2024-09-21 21:24:43 +0900

    Remove test code

--- src/main/java/kr/vanilage/main/discord/ConnectDiscord.kt
+++ src/main/java/kr/vanilage/main/discord/ConnectDiscord.kt
@@ -88,30 +88,4 @@ class ConnectDiscord : Listener {
 
         client.send(message)
     }
-
-    @EventHandler
-    fun onClearAdvancements(e : PlayerAdvancementDoneEvent) {
-        val webhookURL = Main.configuration.getString("WEBHOOK_URL")
-
-        val builder = WebhookClientBuilder(webhookURL!!)
-
-        builder.setThreadFactory { job ->
-            val thread = Thread(job)
-            thread.name = "Hello"
-            thread.isDaemon = true
-            thread
-        }
-
-        builder.setWait(true)
-
-        val client = builder.build()
-
-        val message = WebhookMessageBuilder()
-            .setUsername("${e.player.name} 님이 발전 과제를 달성했습니다.")
-            .setAvatarUrl("https://cravatar.eu/helmhead/${e.player.name}/600.png")
-            .setContent("**${e.advancement.key.key}**")
-            .build()
-
-        client.send(message)
-    }
 }
