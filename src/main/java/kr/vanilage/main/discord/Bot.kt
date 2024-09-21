e2cf715511b9f4f860e8cc09606c3106dc8c4545
MaIcer <116092283+MAKEREKAM@users.noreply.github.com>
2024-09-21 16:19:24 +0900

    Add discord message listener

--- src/main/java/kr/vanilage/main/discord/Bot.kt
+++ src/main/java/kr/vanilage/main/discord/Bot.kt
@@ -3,10 +3,16 @@ package kr.vanilage.main.discord
 import kr.vanilage.main.Main
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Bukkit
+import java.text.SimpleDateFormat
+import java.util.Date
 
 object Bot {
     private lateinit var token : String
@@ -14,16 +20,22 @@ object Bot {
     lateinit var adminChannelId : String
     lateinit var jda : JDA
 
+    lateinit var messageFormat : String
+
     fun run() {
         try {
             token = Main.configuration.getString("BOT_TOKEN")!!
             channelId = Main.configuration.getString("CHANNEL_ID")!!
             adminChannelId = Main.configuration.getString("ADMIN_CHANNEL_ID")!!
 
+            messageFormat = Main.configuration.getString("MINECRAFT_MESSAGE_FORMAT")!!
+
             jda = JDABuilder.createDefault(token)
                 .addEventListeners(
-                    EnableListener()
+                    EnableListener(),
+                    MessageListener()
                 )
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build()
 
             val commands = jda.updateCommands()
@@ -44,5 +56,31 @@ object Bot {
 class EnableListener : ListenerAdapter() {
     override fun onReady(event : ReadyEvent) {
         event.jda.getTextChannelById(Bot.adminChannelId)!!.sendMessage("## SERVER OPEN.").queue()
+    }
+}
+
+class MessageListener : ListenerAdapter() {
+    override fun onMessageReceived(event : MessageReceivedEvent) {
+        if (event.author.isBot) return
+        if (event.message.contentDisplay == "") return
+
+        if (event.channel.id == Bot.channelId) {
+            val message = Bot.messageFormat
+                .replace("\$author", event.author.effectiveName)
+                .replace("\$username", event.author.name)
+                .replace("\$id", event.author.id)
+                .replace("\$time", getTime())
+                .replace("\$content", event.message.contentDisplay)
+
+            val component = MiniMessage.miniMessage().deserialize(message)
+
+            Bukkit.broadcast(component)
+        }
+    }
+
+    private fun getTime() : String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+        return dateFormat.format(Date())
     }
 }
