a4da7d3fa3535d19e0acdab8141d531890aae358
MaIcer <116092283+MAKEREKAM@users.noreply.github.com>
2024-09-22 10:51:55 +0900

    Add enable and disable event

--- src/main/java/kr/vanilage/main/discord/ConnectDiscord.kt
+++ src/main/java/kr/vanilage/main/discord/ConnectDiscord.kt
@@ -9,6 +9,8 @@ import org.bukkit.event.player.PlayerAdvancementDoneEvent
 import org.bukkit.event.player.PlayerChatEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.event.server.PluginDisableEvent
+import org.bukkit.event.server.PluginEnableEvent
 
 class ConnectDiscord : Listener {
     @EventHandler
@@ -83,6 +85,58 @@ class ConnectDiscord : Listener {
         val message = WebhookMessageBuilder()
             .setUsername("${e.player.name} 님이 퇴장하셨습니다.")
             .setAvatarUrl("https://cravatar.eu/helmhead/${e.player.name}/600.png")
+            .setContent("­")
+            .build()
+
+        client.send(message)
+    }
+
+    @EventHandler
+    fun onEnable(e : PluginEnableEvent) {
+        val webhookURL = Main.configuration.getString("WEBHOOK_URL")
+
+        val builder = WebhookClientBuilder(webhookURL!!)
+
+        builder.setThreadFactory { job ->
+            val thread = Thread(job)
+            thread.name = "Hello"
+            thread.isDaemon = true
+            thread
+        }
+
+        builder.setWait(true)
+
+        val client = builder.build()
+
+        val message = WebhookMessageBuilder()
+            .setUsername("서버가 시작되었습니다.")
+            .setAvatarUrl("https://media.discordapp.net/attachments/1280076410081251419/1282197255335444542/image.png")
+            .setContent("­")
+            .build()
+
+        client.send(message)
+    }
+
+    @EventHandler
+    fun onDisable(e : PluginDisableEvent) {
+        val webhookURL = Main.configuration.getString("WEBHOOK_URL")
+
+        val builder = WebhookClientBuilder(webhookURL!!)
+
+        builder.setThreadFactory { job ->
+            val thread = Thread(job)
+            thread.name = "Hello"
+            thread.isDaemon = true
+            thread
+        }
+
+        builder.setWait(true)
+
+        val client = builder.build()
+
+        val message = WebhookMessageBuilder()
+            .setUsername("서버가 종료되었습니다.")
+            .setAvatarUrl("https://media.discordapp.net/attachments/1280076410081251419/1282197255335444542/image.png")
             .setContent("­")
             .build()
 
